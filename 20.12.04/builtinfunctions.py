59e5ba061a2c7d9c694d3562fea69cd61ba236bb
한상현 <kerter48@naver.com>
2020-12-04 14:31:42 +0000

    20.12.04

---
+++ 20.12.04/builtinfunctions.py
@@ -0,0 +1,12 @@
+import builtins
+
+builtins.globals()
+
+ranking = {
+    'A' : 100,
+    'B' : 85,
+    'C' : 95
+}
+
+#ranking.get('A')
+print(sorted(ranking, key = ranking.get, reverse = True))
---
+++ 20.12.04/import.py
@@ -0,0 +1,14 @@
+import collections #표준 라이브러리
+import os
+import sys
+
+import termcolor #서드파티 라이브러리
+
+#import python_package #다른 사람의 라이브러리,패키지,모듈
+
+#import config #자신의 라이브러리,패키지,모듈
+
+print(collections.__file__)
+print(termcolor.__file__)
+
+print(sys.path)
